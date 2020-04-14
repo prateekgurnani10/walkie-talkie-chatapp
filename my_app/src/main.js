cd426ac5e538f849218cbce228e1a773b9e7dd73
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-04-14 06:33:37 -0400

    🐛 FIX: Configured home route

--- my_app/src/main.js
+++ my_app/src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { firestorePlugin } from 'vuefire'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false
+import Home from './components/Home'
 
+const router = new VueRouter({
+  routes: [{path:'/', component:Home},]
+})
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
