7be12fc80b44cd9b9970de3439e38273cee768e1
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-04-14 03:41:44 -0400

    🐛 FIX: Edited the App.vue

--- my_app/src/App.vue
+++ my_app/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1> Vue Voxer </h1>
+      <p>RealTime voice chat </p>
+    </header>
+
   </div>
 </template>
 
@@ -11,18 +14,13 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
+
 </style>
