de43090ee0dc1ab7f7f45e0a802121f6a610293a
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-04-15 02:38:58 -0400

    📦 NEW: Added the Sign in button.

--- my_app/src/components/Home.vue
+++ my_app/src/components/Home.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
         <h3>Home</h3>
+        <Login/>
     </div>
+
 </template>
+
+<script>
+
+import login from "./login"
+export default {
+    components:{
+        Login
+    }
+
+}
+</script>
