5083f31a256212c02f81c09f4f3a9adfa04f0f3f
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-04-15 06:19:12 -0400

    📦 NEW:  added the user component

---
+++ my_app/src/components/User.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <slot name="user" :user="user"  ></slot>
+    </div>
+</template>
+<script>
+import {ref} from '@vue/composition-api'
+export default {
+    setup(){
+        const user = ref(null);
+        const unsubscribe = auth.onAuthStateChanged(
+                firebaseUser => user.value = firebaseUser
+        );
+        return{
+            user,
+            unsubscribe,
+        }
+    },
+    destroyed() {
+        this.unsubscribe()
+    }
+
+}
+</script>
