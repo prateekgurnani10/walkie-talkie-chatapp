6ca2e65c702761688ab7dbd4564efcc5c7f991af
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-04-15 02:38:27 -0400

    📦 NEW: Added anonymous authentication

---
+++ my_app/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <aside>
+        <h3>
+            Sign in Anonymously
+        </h3>
+        <button @click="auth.signinAnonymously()">Sign in</button>
+    </aside>
+</template>
+<script>
+import auth from "../firebase"
+export default {
+    data(){
+        return {
+            auth,
+        }
+    },
+
+}
+</script>
