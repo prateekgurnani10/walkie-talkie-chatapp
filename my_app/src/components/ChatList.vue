b01ead35965c23643183bab731ba842ed4d3df17
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-04-19 18:43:06 -0400

    👌 IMPROVE: refactored

--- my_app/src/components/ChatList.vue
+++ my_app/src/components/ChatList.vue
@@ -1,25 +1,38 @@
 <template>
-    <div>
-        <button @click= "createChatRoom"> create chatroom</button>
-    </div>
+  <div>
+    <button @click="createChatRoom()" class="button">Create New Chat Room</button>
+  </div>
 </template>
+
 <script>
+import { db } from '../firebase';
 
-import {db} from '../firebase';
 export default {
-
-    methods: {
-        asyn createChatRoom() {
-            const newChat = await db.collection('chats').add({
-                createdAt: Date.now(),
-                owner: this.uid,
-                members: [this.uid]
-            })
+  data() {
+    return {
+        chats: []
+    }
+  },
+  firestore() {
+    return {
+        chats: db.collection('chats').where('owner', '==', this.uid)
+    }
+  },
+  methods: {
+      async createChatRoom() {
+          const newChat = await db.collection('chats').add({
+              createdAt: Date.now(),
+              owner: this.uid,
+              members: [this.uid]
+          })
 
             console.log(newChat)
-        }
-    },
-    props: ['uid']
+      }
 
-}
+
+
+  },
+  props: ['uid']
+
+};
 </script>
