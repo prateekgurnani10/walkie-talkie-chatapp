45126f4e778fbaebdfb2469fb06c1e80ba480d38
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-04-19 18:33:56 -0400

    📦 NEW: added chatlist component

---
+++ my_app/src/components/ChatList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button @click= "createChatRoom"></button>
+    </div>
+</template>
+<script>
+
+import {db} from '../firebase';
+export default {
+
+    methods: {
+        asyn createChatRoom() {
+            const newChat = await db.collection('chats').add({
+                createdAt: Date.now(),
+                owner: this.uid,
+                members: [this.uid]
+            })
+
+            console.log(newChat)
+        }
+    },
+    props: ['uid']
+
+}
+</script>
