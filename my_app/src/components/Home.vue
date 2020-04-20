8d66ce1c87ebb74622f1a8c0c1254cc49a82d08b
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-04-20 02:45:17 -0400

    🐛 FIX: edited

--- my_app/src/components/Home.vue
+++ my_app/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class = "section">
-        <h2>Home.vue</h2>
+        <h2></h2>
         <User v-slot:user="{user}">
             <div v-if="user">
                 <h3>User Profile </h3>
