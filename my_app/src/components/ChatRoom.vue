c9700ba893ac8bcc44b66ba67fc4fbcd6b7314ee
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-04-20 00:59:17 -0400

    📦 NEW: added chatroom component

---
+++ my_app/src/components/ChatRoom.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="section">
+    <h3>Welcome to ChatRoom.vue {{ chatId }}</h3>
+
+    <router-link to="/">Back</router-link>
+
+  </main>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    chatId() {
+      return this.$route.params.id;
+    },
+  },
+
+};
+</script>
